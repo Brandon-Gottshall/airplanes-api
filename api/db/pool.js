80d9f9abac874b0d0cf7aad00e640e74cda128c8
Brandon🤖 <brandon@effortlessdevelopment.com>
2023-04-13 20:45:28 -0400

    change structure

--- api/db/pool.js
+++ api/db/pool.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   user: "postgres",
-  host: "airplanes-api.c1h1sdcooewq.us-east-1.rds.amazonaws.com",
-  password:'nebulanebula',
+  host: "containers-us-west-137.railway.app",
+  password:'0zcqleJbblkd2o0Sbkmm',
   database: "airplanes",
-  port: 5432
+  port: 6662
 })       
 
 module.exports = () => pool;
