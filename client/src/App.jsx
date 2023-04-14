07b6a1ca54e62262b2dbc43f90efc55bdef77e4f
Brandon🤖 <brandon@effortlessdevelopment.com>
2023-04-13 22:26:48 -0400

    endpoint Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,20 +23,20 @@ function App() {
   const toggleCreationBlock = () => setShowCreationBlock(!showCreationBlock)
 
   const fetchPlanes = async () => {
-    const response = await fetch('http://localhost:3001/planes')
+    const response = await fetch('https://d3haiuftxr26lv.cloudfront.net/planes')
     const data = await response.json()
     return data
   }
 
   
   const fetchFlights = async () => {
-    const response = await fetch('http://localhost:3001/flights')
+    const response = await fetch('https://d3haiuftxr26lv.cloudfront.net/flights')
     const data = await response.json()
     setFlightArray(data)
   }
   
   const deleteFlight = async (flightcod) => {
-    const response = await fetch(`http://localhost:3001/flight/${flightcod}`, { method: 'DELETE' })
+    const response = await fetch(`https://d3haiuftxr26lv.cloudfront.net/flight/${flightcod}`, { method: 'DELETE' })
     const data = await response
   }
 
